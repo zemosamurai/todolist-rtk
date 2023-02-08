85ff103e2c1cb71d6c993181a71fb351741d9084
zemosamurai <zemosamurai@gmail.com>
2023-02-08 10:36:57 +0300

    add error handler

--- src/utils/utils-error.ts
+++ src/utils/utils-error.ts
@@ -1,3 +1,17 @@
-export const appNetworkError = () => {
+import {setError, setLoading} from "../store/slice/appSlice";
+import {AppDispatchType} from "../store/store";
+import {ResponseType} from "../api/todolist-api";
 
+export const handleServerAppError = <T>(dispatch: AppDispatchType, data: ResponseType<T>) => {
+    if (data.messages.length) {
+        dispatch(setError({error: data.messages[0]}))
+    } else {
+        dispatch(setError({error: 'Some Error'}))
+    }
+    dispatch(setLoading({status: 'failed'}))
+}
+
+export const handleServerNetworkError = (dispatch: AppDispatchType, error: string) => {
+    dispatch(setError({error: error}))
+    dispatch(setLoading({status: 'failed'}))
 }
