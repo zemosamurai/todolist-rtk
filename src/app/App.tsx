328b0703562a2410a261ded584148ea4a5950d6c
zemosamurai <zemosamurai@gmail.com>
2023-02-10 16:43:14 +0300

    add Routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,7 +9,12 @@ function App() {
         <div className="App">
             <AppBarHeader/>
             <Container fixed>
-                <TodolistList/>
+                <Routes>
+                    <Route path={'/'} element={<TodolistList/>}/>
+                    <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'/404'} element={<h1>404 NOT FOUND</h1>}/>
+                    <Route path={'*'} element={<Navigate to={'/404'}/>}/>
+                </Routes>
             </Container>
         </div>
     );
