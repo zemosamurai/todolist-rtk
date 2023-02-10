a08386666a6bea2e8dbf2bf71777df3e4f14889e
zemosamurai <zemosamurai@gmail.com>
2023-02-10 16:40:39 +0300

    add redirect

--- src/components/TodolistList/TodolistList.tsx
+++ src/components/TodolistList/TodolistList.tsx
@@ -4,47 +4,57 @@ import Paper from "@mui/material/Paper";
 import {Todolist} from "./Todolist/Todolist";
 import React, {useCallback, useEffect} from "react";
 import {
-    addTodolistTC, changeTodolistTitleTC, fetchTodolistsTC, FilterValueType,
-    removeTodolistTC, TodolistDomainType, todolistSlice,
-} from "../../store/reducers/todolistSlice";
+    addTodolistTC, changeTodoFilter, changeTodolistTitleTC, fetchTodolistsTC, FilterValueType,
+    removeTodolistTC, TodolistDomainType,
+} from "../../store/slice/todolistSlice";
 import {useAppDispatch, useAppSelector} from "../../store/hooks";
-import {addTasksTC, deleteTaskTC, TaskStateType, updateTaskTC} from "../../store/reducers/taskSlice";
+import {addTasksTC, deleteTaskTC, TaskStateType, updateTaskTC} from "../../store/slice/taskSlice";
 import {TaskStatuses} from "../../api/todolist-api";
+import {Navigate} from "react-router-dom";
 
 export const TodolistList = () => {
     const dispatch = useAppDispatch()
     let todolists = useAppSelector<TodolistDomainType[]>(state => state.todolist.todolists)
     let tasks = useAppSelector<TaskStateType>(state => state.task.tasks)
+    let isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
 
     useEffect(() => {
+        if (!isLoggedIn) {
+            return
+        }
         dispatch(fetchTodolistsTC())
     }, [])
 
     const removeTask = useCallback((todoId: string, taskId: string) => {
-        dispatch(deleteTaskTC(todoId, taskId))
+        dispatch(deleteTaskTC({todoId, taskId}))
     }, [])
     const addTask = useCallback((todoId: string, title: string) => {
-        dispatch(addTasksTC(todoId, title))
+        dispatch(addTasksTC({todoId, title}))
     }, [])
     const changeTaskStatus = useCallback((todoId: string, taskId: string, status: TaskStatuses) => {
-        dispatch(updateTaskTC(todoId, taskId, {status}))
+        dispatch(updateTaskTC({todoId, taskId, domainModel: {status}}))
     }, [])
     const changeTaskTitle = useCallback((todoId: string, taskId: string, title: string) => {
-        dispatch(updateTaskTC(todoId, taskId, {title}))
+        dispatch(updateTaskTC({todoId, taskId, domainModel: {title}}))
     }, [])
 
+
     const changeFiler = useCallback((todoId: string, filter: FilterValueType) => {
-        dispatch(todolistSlice.actions.changeFiler({todoId, filter}))
+        dispatch(changeTodoFilter({todoId, filter}))
     }, [])
     const removeTodoList = useCallback((todoId: string) => {
         dispatch(removeTodolistTC(todoId))
     }, [])
     const changeTodolistTitle = useCallback((todoId: string, title: string) => {
-        dispatch(changeTodolistTitleTC(todoId, title))
+        dispatch(changeTodolistTitleTC({todoId, title}))
     }, [])
     const addTodolist = useCallback((title: string) => {
         dispatch(addTodolistTC(title))
     }, [])
+
+    if (!isLoggedIn) {
+        return <Navigate to={'/login'}/>
+    }
 
     return <>
         <Grid container style={{margin: '30px 0'}}>
@@ -67,6 +77,7 @@ export const TodolistList = () => {
                             changeFiler={changeFiler}
                             removeTodoList={removeTodoList}
                             changeTodolistTitle={changeTodolistTitle}
+                            entityStatus={tl.entityStatus}
                         />
                     </Paper>
                 </Grid>
