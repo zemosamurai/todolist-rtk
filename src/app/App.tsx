01de25041371b4b2d64fca54053e4a4a981c9efe
zemosamurai <zemosamurai@gmail.com>
2023-02-10 16:44:01 +0300

    add Preloads and isLoginIn?

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {AppBarHeader} from "../components/AppBar/AppBar";
 import Container from "@mui/material/Container";
 import {TodolistList} from "../components/TodolistList/TodolistList";
+import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Login} from "../features/Login/Login";
+import {meTC} from "../store/slice/authSlice";
+import {useAppDispatch, useAppSelector} from "../store/hooks";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function App() {
+    const dispatch = useAppDispatch()
+    const isInitialized = useAppSelector<boolean>(state => state.app.isInitialized)
+
+    useEffect(() => {
+        dispatch(meTC())
+    }, [])
+
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+    }
+
     return (
         <div className="App">
+            <ErrorSnackbar/>
             <AppBarHeader/>
             <Container fixed>
                 <Routes>
